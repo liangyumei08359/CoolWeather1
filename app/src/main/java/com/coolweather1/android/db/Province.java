e2b72b9a7695244f4baa660dc2a49014b39aaa77
liangyumei08359 <1093841033@qq.com>
2017-12-13 21:45:03 +0800

    加入创建数据库和表的各种配置

--- app/src/main/java/com/coolweather1/android/db/Province.java
+++ app/src/main/java/com/coolweather1/android/db/Province.java
@@ -14,13 +14,13 @@ public class Province extends DataSupport {
         return id;
     }
     public void setId(int id){
-        this.id=id;
+        this.id = id;
     }
     public String getProvinceName(){
         return provinceName;
     }
     public void setProvinceName(String provinceName){
-        this.provinceName=provinceName;
+        this.provinceName = provinceName;
     }
     public  int getProvinceCode(){
         return provinceCode;
